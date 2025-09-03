03c78c6f56ba4c06237c123c9e7f4f35801f6653
Sudhanshu Mahle <sudhanshumahle748@gmail.com>
2025-09-03 22:54:01 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const initdata = require("../init/data");
 const Listing = require("../models/listing.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/AirBnb";
+const MONGO_URL = "mongodb+srv://entertainmenttiktok42_db_user:WugNmnox1WZJnJDq@cluster0.wattrjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Function to connect to the database
 async function connectDB() {
